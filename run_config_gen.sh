d7c1b235dd248e6474871d9b62651baf14439b7d
Monica Rotulo <monica.rotulo@gmail.com>
2024-04-17 16:16:46 +0200

    minor path updates in the sh

---
+++ run_config_gen.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=gen_config            # Job name
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=128
+#SBATCH --time=00:30:00               # Time limit hrs:min:sec
+#SBATCH --partition=rome
+
+SPPARKS="${HOME}/spparks"
+OUTPUT="${SPPARKS}/$(date +%Y-%m-%d_%H-%M-%S)_${SLURM_JOBID}"
+mkdir -p $OUTPUT
+
+# Activate environment and load necessary modules
+source ../venv/bin/activate
+module load 2022 
+module load Python/3.10.4-GCCcore-11.3.0
+
+# copy relevant files 
+cp ./config/param_space.yaml "$TMPDIR"
+
+python config_file_generator.py --yaml_file "$TMPDIR"/param_space.yaml --output_dir "$TMPDIR"
+
+cp "$TMPDIR"/param_space.yaml $OUTPUT
+cp -r "$TMPDIR"/config_file_* $OUTPUT
